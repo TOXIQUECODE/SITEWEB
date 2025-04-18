c0ffb3c6ad53734aee9208cd97654ec356b02832
TOXIQUECODE <moucherouge28@gmail.com>
2025-04-17 20:43:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const https = require('https');
 const WebSocket = require('ws');
 const express = require('express');
 const app = express();
 
 // Créer un serveur HTTPS avec Express (Render gère les certificats SSL automatiquement)
-const server = https.createServer(app);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Serveur démarré sur le port ${process.env.PORT}`);
+});
 
 // Créer un serveur WebSocket sécurisé (WSS) en associant avec le serveur HTTPS
 const wss = new WebSocket.Server({ server });
@@ -19,17 +20,10 @@ wss.on('connection', (ws) => {
   });
 });
 
-// Lancer le serveur HTTPS sur le port 443 (port HTTPS par défaut)
-const port = process.env.PORT || 443; // Utiliser le port fourni par Render
-server.listen(port, () => {
-  console.log(`Serveur démarré sur le port ${port}`);
-});
-
-
 // Servir les fichiers statiques dans le dossier 'public'
 app.use(express.static('public'));
 
-// Exemple de route, tu peux personnaliser en fonction de tes besoins
+// Exemple de route
 app.get('/', (req, res) => {
   res.send('Bienvenue sur le serveur sécurisé !');
 });
