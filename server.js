ca2654420f68031339257fb619359d8d8d947c52
TOXIQUECODE <moucherouge28@gmail.com>
2025-04-18 05:42:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3000;
 
 // Configuration pour __dirname avec ES Modules
 const __filename = fileURLToPath(import.meta.url);
